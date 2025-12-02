39d3daae4bad1f77544289b52b300179a9022a07
Arsenii Buriak <128735775+SenchaBka@users.noreply.github.com>
2025-12-02 15:59:37 -0500

    Additions to dealership info

--- backend/services/run_build_dealer.py
+++ backend/services/run_build_dealer.py
@@ -1,4 +1,4 @@
 from build_vectorstore import build_vectorstore_from_argument
 
-text = "Your are North Auto dealership. You sell variety of Volvo cars."
+text = "Your are North Auto dealership. You sell variety of Volvo cars. You are located at Volvostreet 123, North York. Your working hours are 9am to 6pm Monday to Friday. Contact number is +1-437-999-9999."
 build_vectorstore_from_argument(text)
